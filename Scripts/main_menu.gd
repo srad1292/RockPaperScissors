fa02b71564d93c5ea69d296dd49c01fe6b42b477
Sam Radford <smr1292@yahoo.com>
2024-01-17 14:44:05 -0500

    First commit, some simply UI work to figure things out.

---
+++ Scripts/main_menu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+
+func _on_btn_play_pressed():
+	pass # Replace with function body.
+
+
+func _on_btn_characters_pressed():
+	var char_menu = load("res://Scenes/characters_menu.tscn").instantiate()
+	get_tree().current_scene.add_child(char_menu)
+
+func _on_btn_quit_pressed():
+	get_tree().quit()
